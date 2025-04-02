442d7f589b72f658f51a7e3df98b13474a01f5af
a2layuesun <a2@layuesun.store>
2025-04-02 17:38:19 +0700

    Delete cloaking.php

--- cloaking.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!isset($_SERVER['HTTP_USER_AGENT']) || strpos($_SERVER['HTTP_USER_AGENT'], 'Bot') === false) {
-    header('Location: http://example.com/landing-page');
-    exit;
-} else {
-    header('Location: http://example.com/error-page');
-    exit;
-}
-?>
